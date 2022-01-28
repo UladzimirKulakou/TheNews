9ff7f9447c2b5f2f85aa80c117ca2affcfe52488
Uladzimir Kulakou <rombik9@ya.ru>
2022-01-28 16:42:59 +0300

    commit 3
    it is work

---
+++ TheNews/TheNews/NewsTableViewCell.swift
@@ -0,0 +1,112 @@
+//
+//  NewsTableViewCell.swift
+//  TheNews
+//
+//  Created by Uladzimir Kulakou on 1/28/22.
+//
+
+import UIKit
+
+class NewTableViewCellViewModel {
+    let title: String
+    let subtitle: String
+    let imageURL: URL?
+    var imageData: Data? = nil
+    
+    init(
+        title: String,
+     subtitle: String,
+        imageURL: URL?
+    ) {
+        self.title = title
+        self.subtitle = subtitle
+        self.imageURL = imageURL
+    }
+}
+
+
+class NewsTableViewCell: UITableViewCell {
+
+   static let identifier = "NewsTableViewCell"
+    
+    private let newsTitleLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.font = .systemFont(ofSize: 22, weight: .medium)
+        return label
+    
+    }()
+    private let subtitleLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.font = .systemFont(ofSize: 15, weight: .regular)
+        return label
+    
+    }()
+    
+    private let newsImageVeiw: UIImageView = {
+       let imageView = UIImageView()
+        imageView.clipsToBounds = true
+        imageView.backgroundColor = .systemBlue
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?){
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        contentView.addSubview(newsTitleLabel)
+        contentView.addSubview(subtitleLabel)
+        contentView.addSubview(newsImageVeiw)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        newsTitleLabel.frame = CGRect(x: 10,
+                                      y: 0,
+                                      width: contentView.frame.size.width - 170,
+                                      height: 70)
+        subtitleLabel.frame = CGRect(x: 10,
+                                      y: 70,
+                                      width: contentView.frame.size.width - 170,
+                                      height: contentView.frame.size.height/2)
+        newsImageVeiw.frame = CGRect(x: contentView.frame.width - 160,
+                                      y: 5,
+                                      width: 160,
+                                      height: contentView.frame.size.height - 10)
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        newsTitleLabel.text = nil
+        subtitleLabel.text = nil
+        newsImageVeiw.image = nil  
+    }
+    func configure(with viewModel: NewTableViewCellViewModel){
+        newsTitleLabel.text = viewModel.title
+        subtitleLabel.text = viewModel.subtitle
+        
+        
+        // image
+        
+        if let data = viewModel.imageData {
+            newsImageVeiw.image = UIImage(data: data)
+        }
+        else if let url = viewModel.imageURL{
+            URLSession.shared.dataTask(with: url) {
+               [weak self] data, _, error in
+                guard let data = data, error == nil else {
+                    return
+                }
+                viewModel.imageData = data
+                DispatchQueue.main.async {
+                    self?.newsImageVeiw.image = UIImage(data: data)
+                }
+            }.resume()
+            //fetch
+        }
+        
+    }
+}
